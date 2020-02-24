9781c36cdd8fe2bf5b0c41377d07006ac4532b8e
Сергей_Я <tooniccee@gmail.com>
2020-02-24 12:11:39 +0800

    add

--- stopwatch/src/components/Tabs/Tabs.jsx
+++ stopwatch/src/components/Tabs/Tabs.jsx
@@ -6,10 +6,11 @@ import Countdown from '../Countdown';
 
 import 'antd/dist/antd.css';
 
-const Tab = () => { const { TabPan  = Tabs;
+const Tab = () => {
+  const { TabPan } = Tabs;
   return (
-    <Tabs defaultActiveKey=1">
-      TabPane tab="Timer" key="1">
+    <Tabs defaultActiveKey="1">
+      <TabPane tab="Timer" key="1">
         <Timer />
       </TabPane>
       <TabPane tab="Cuntdown" key="2">
